8fe10bba7bdd1c48eaca04bdf7d92efa62ea4480
Rony Hanna <142740327+R-Hanna@users.noreply.github.com>
2023-08-21 14:31:00 +0300

    Update InetAddress.java

--- InetAddress/InetAddress.java
+++ InetAddress/InetAddress.java
@@ -1,1 +1,25 @@
+import java.net.*;
+
+public class InetAddress {
+
+    public static void main(String[] args) {
+        try {
+            InetAddress adrLocale = InetAddress.getLocalHost();
+            System.out.println("Adresse Locale = "
+                              + adrLocale.getHostAddress() );
+            
+            InetAddress adrServeur = InetAddress.getByName("google.com");
+            System.out.println("Adresse Google = "
+                              + adrServeur.getHostAddress() );
+            
+            InetAddress[] adrServeurs = InetAddress.getAllByName("www.microsoft.com");
+            System.out.println("Adresse Microsoft= ");
+            for (int i = 0; i < adrServeurs.length; i++) {
+                System.out.println(" " + adrServeurs[i].getHostAddress());
+            }
+        } catch (UnknownHostException e) {
+        }
+    }
+
+}
 
