b62fca8aac10f8b853b2258e7a50213d98f53a63
Rony Hanna <142740327+R-Hanna@users.noreply.github.com>
2023-08-21 14:46:38 +0300

    Update URLEncode.java

--- URLEncode/URLEncode.java
+++ URLEncode/URLEncode.java
@@ -1,1 +1,16 @@
+import java.net.*;
+import java.io.UnsupportedEncodingException;
 
+public class URLEncode {
+
+    public static void main(String[] args) {
+        try {
+            String url = "http://www.test.fr/images perso/mon image.gif";
+            System.out.println(URLEncoder.encode(url, "UTF-8"));
+            System.out.println(URLEncoder.encode(url, "UTF-16"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
