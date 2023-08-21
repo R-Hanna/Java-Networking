b4fbf3abe129d71840424dac97a6527f1609a629
Rony Hanna <142740327+R-Hanna@users.noreply.github.com>
2023-08-21 15:08:38 +0300

    Update Server.java

--- Remote Method Invocation/Server.java
+++ Remote Method Invocation/Server.java
@@ -1,1 +1,17 @@
+import java.rmi.*;
+import java.rmi.registry.*;
 
+public class Server {
+
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099);
+            System.setProperty("java.rmi.server.hostname", "192.168.1.171");
+            WelcomeImplementation localObject = new WelcomeImplementation();
+            String url = "rmi://localhost/WelcomeInterface";
+            Naming.rebind(url, localObject);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
